537833938d87dbad3f1146af2dcbcc8e9f3a8b4a
Ingrid <Ingridmalik668@hotmail.com>
2025-06-20 17:56:39 -0300

    ✅ Update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ListaDeTarefas/', // <-- ESSA LINHA é essencial para GitHub Pages
+  base: '/ListaDeTarefas/',
   plugins: [react()],
 })
